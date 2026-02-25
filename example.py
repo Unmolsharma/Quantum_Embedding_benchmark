b35f3e3226c9c324692044b071524c3c12e7d7cd
Zach Macsmith <zachmacsmith@gmail.com>
2026-02-25 15:26:41 -0600

    added benchmarking_algorithms

---
+++ example.py
@@ -0,0 +1,17 @@
+from qebench import EmbeddingBenchmark
+from qeanalysis import BenchmarkAnalysis
+
+
+bench = EmbeddingBenchmark(target_graph=None)
+direct = bench.run_full_benchmark(
+    graph_selection='100-130',
+    topologies=['chimera_4x4x4'],
+    methods=['minorminer','clique','atom','oct-triad','oct-triad-reduce','oct-fast-oct','oct-fast-oct-reduce'],
+    n_trials=5,
+    warmup_trials=1,
+    timeout=60,
+    batch_note='Random graphs 100-199 on chimera_4x4x4',
+)
+
+an = BenchmarkAnalysis(direc)
+an.generate_report()
